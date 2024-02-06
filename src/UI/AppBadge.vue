b49f81556866f98fefac24103fb02570e961d209
NikitaTerekhin <nterekhin@hiypcorp.com>
2024-02-06 10:20:34 +0300

    UI component test

---
+++ src/UI/AppBadge.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="app-badge">
+        {{ title }}
+    </div>
+</template>
+
+<script lang="ts" setup>
+import type {PropType} from "vue";
+import {defineProps} from "vue";
+
+type ColorType = 'red' | 'grey' | 'green' | 'blue';
+
+const props = defineProps({
+    title: {
+        type: String as PropType<string>,
+        required: true,
+    },
+    color: {
+        type: String as PropType<ColorType>,
+        required: true,
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.app-badge {
+    height: 20px;
+    width: 57px;
+    border-radius: 10px;
+}
+</style>
