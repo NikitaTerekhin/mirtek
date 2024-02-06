b4cf16c1357c43d9c9f611d1fa4e2c44900b33de
NikitaTerekhin <nterekhin@hiypcorp.com>
2024-02-06 13:02:56 +0300

    components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,49 @@
 <template>
     <header class="bg-white shadow-lg">
-        <div class="container mx-auto flex items-center justify-between py-4 px-6">
+        <div class="container mx-auto flex items-center justify-between py-4 px-6 h-16">
             <div class="flex items-center space-x-4">
-                <RouterLink
-                    v-for="link in links"
-                    :key="link.id"
-                    :to="link.url"
-                    class="text-gray-600 hover:text-gray-800"
-                    :class="{ 'text-blue-500': $route.name === link.name.toLowerCase() }"
-                >
-                    {{ link.name }}
-                </RouterLink>
+                <template v-if="$route.name !== 'dashboard'">
+                    <RouterLink
+                        v-for="link in links"
+                        :key="link.id"
+                        :to="link.url"
+                        class="text-gray-600 hover:text-gray-800"
+                        :class="{ 'text-blue-500': $route.name === link.name.toLowerCase() }"
+                    >
+                        {{ link.name }}
+                    </RouterLink>
+                </template>
+
+                <AppInput
+                    v-if="store.getters.isAuth && $route.name === 'dashboard'"
+                />
             </div>
 
             <div class="flex items-center space-x-2">
-                <button
-                    class="text-gray-600 hover:text-gray-800 flex items-center"
-                    @click="navigateToPage('/login')"
-                >
-                    <img src="../assets/icons/login.svg" alt="#" class="px-2">
-                    <span>Login/Register</span>
-                </button>
+                <template v-if="!store.getters.isAuth">
+                    <button
+                        class="text-gray-600 hover:text-gray-800 flex items-center"
+                        @click="login"
+                    >
+                        <img src="../assets/icons/login.svg" alt="#" class="px-2">
+                        <span>Login/Register</span>
+                    </button>
+                </template>
+
+                <template v-else>
+                    <img src="../assets/icons/bell.svg" alt="#" class="px-2">
+                    <img src="../assets/icons/avatar.svg" alt="">
+                </template>
             </div>
         </div>
     </header>
 </template>
 
 <script lang="ts" setup>
-import {reactive} from "vue";
-import {useRouter } from 'vue-router';
+import { reactive } from "vue";
 import { useStore } from 'vuex';
+import AppInput from "../UI/AppInput.vue";
 
-const router = useRouter();
 const store = useStore();
 
 const links = reactive([
@@ -41,7 +53,7 @@ const links = reactive([
     {id: 4, name: 'Calendar', url: '/calendar'},
 ]);
 
-const navigateToPage = (url: string) => {
-    router.push(url);
+const login = () => {
+    store.commit('setIsAuth', true);
 }
 </script>
